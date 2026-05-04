08f8a89b490ca2780f87a6b469ab33598e1cceac
rudney5000 <rudneyngueko@gmail.com>
2026-05-04 19:03:27 +0300

    chore: vite and ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path/win32'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
 })
