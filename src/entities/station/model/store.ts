dceb7798c2c9cc1955df10e2aca97abdea19aeae
rudney5000 <rudneyngueko@gmail.com>
2026-05-06 03:12:14 +0300

    feat: add actions of sidebar and stats panel

--- src/entities/station/model/store.ts
+++ src/entities/station/model/store.ts
@@ -1,4 +1,6 @@
 import {defineStore} from "pinia";
+import { stationsRaw as staticStations} from '@/shared/data/stations.ts'
+import { lines as staticLines} from '@/shared/data/lines.ts'
 import type {IsochroneFeature, LineId, MetroLine, Station} from "@/entities/station/model/types.ts";
 
 export const useMapStore = defineStore('map', {
@@ -11,26 +13,63 @@ export const useMapStore = defineStore('map', {
 
         intervals: [15, 30, 60],
         isochrones: null as IsochroneFeature | null,
-        isochronesLoading: false
+        isochronesLoading: false,
+
+        sidebarOpen: true,
+        statsPanelOpen: false
     }),
 
     getters: {
         enabledLineSet: (state) => new Set(state.enabledLines),
     },
     actions: {
+
+        initStore() {
+            this.stations = staticStations
+            this.lines = staticLines
+            this.enabledLines = staticLines.map(l => l.id as LineId)
+        },
+
+        setLines(lines: MetroLine[]) {
+            this.lines = lines
+        },
+
         setStations(stations: Station[]) {
           this.stations = stations
         },
+
         selectStation(station: Station | null) {
             this.selectedStation = station ? { ...station } : null;
             this.isochrones = null
         },
+
+        setIntervals(intervals: number[]) {
+            this.intervals = intervals
+        },
+
+        toggleLine(line: LineId) {
+          if(this.enabledLines.includes(line)) {
+              this.enabledLines = this.enabledLines.filter(l => l !== line)
+          } else {
+              this.enabledLines.push(line)
+          }
+        },
+
         setIsochrones(iso: IsochroneFeature | null) {
             this.isochrones = iso;
             this.isochronesLoading = false
         },
+
         setIsochronesLoading(loading: boolean){
             this.isochronesLoading = loading
+        },
+
+        toggleSidebar(){
+            this.sidebarOpen = !this.sidebarOpen
+        },
+
+        toggleStatsPanel() {
+            this.statsPanelOpen = !this.statsPanelOpen
         }
     }
 })
