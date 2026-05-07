11d71acd8060b1816c01ee25502ce6f49b364c35
rudney5000 <rudneyngueko@gmail.com>
2026-05-07 10:54:11 +0300

    fix: style of map

--- src/shared/config/map.ts
+++ src/shared/config/map.ts
@@ -1,6 +1,13 @@
 const MAPTILER_KEY = import.meta.env.VITE_MAPTILER_KEY
+
+export const MAP_STYLES = {
+    streets: `https://api.maptiler.com/maps/streets/style.json?key=${MAPTILER_KEY}`,
+    datavizDark: `https://api.maptiler.com/maps/dataviz-dark/style.json?key=${MAPTILER_KEY}`,
+    datavizLight: `https://api.maptiler.com/maps/dataviz-light/style.json?key=${MAPTILER_KEY}`,
+}
+
 export const MAP_CONFIG = {
     center: [37.6173, 55.7558] as [number, number],
     zoom: 11,
-    style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${MAPTILER_KEY}`,
+        style: MAP_STYLES.datavizDark,
 }
